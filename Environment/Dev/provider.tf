4b670a0bb45cdd32d560dff682112b42e3777bb4
Suchita Kanekar <suchitaanup@outlook.com>
2026-09-08 12:18:46 +0530

    modifing backend

--- Environment/Dev/provider.tf
+++ Environment/Dev/provider.tf
@@ -5,15 +5,14 @@ terraform {
       version = "4.48.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "dhondu-rg"
-    storage_account_name = "microdevinfrastorage456"
-    container_name       = "dev-container"
-    key                  = "dev.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "dhondu-rg"
+  #   storage_account_name = "microdevinfrastorage456"
+  #   container_name       = "dev-container"
+  #   key                  = "dev.tfstate"
+  # }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "ed2a265e-c844-4e28-b540-796a000a90d4"
 }
